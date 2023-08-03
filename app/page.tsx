a828d6e687bb9d5e95054581844c4fbda387b330
Duncan Moyo <duncan@darkpools.co.za>
2023-08-03 04:33:33 +0200

    Implementing the authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
+import { db } from "@/lib/db";
 
-
-export default function Home() {
-  return (
-   <div className='text-red-500'>Hello world</div>
-  )
+export default async function Home() {
+  await db.set("hello", "hello");
+  return <div className="text-red-500">Hello world</div>;
 }
