1f9f41e8839abfd3f43319e7cfbd7880c72412f0
Duncan Moyo <duncan@darkpools.co.za>
2023-08-03 16:51:04 +0200

    creating our dashboard layout

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,3 @@
 export {default as AddFriendButton} from './AddFriendButton';
+export {default as SignOutButton} from './SignOutButton';
+export {default as FriendRequestsSidebarOption} from './FriendRequestsSidebarOption';
