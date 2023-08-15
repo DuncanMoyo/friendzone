1f9eb9e81070c61b73756782dd415588699bc3f0
Duncan Moyo <duncan@darkpools.co.za>
2023-08-15 05:55:11 +0200

    Individual chat

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,5 @@ export {default as SignOutButton} from './SignOutButton';
 export {default as FriendRequestsSidebarOption} from './FriendRequestsSidebarOption';
 export {default as FriendRequests} from './FriendRequests';
 export {default as SidebarChatList} from './SidebarChatList';
+export {default as Messages} from './Messages';
+export {default as ChatInput} from './ChatInput';
